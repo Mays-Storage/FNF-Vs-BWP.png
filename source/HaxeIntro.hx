dae373793912b02c7da64cf33306b0fba08ae5d8
MaysLastPlay10 <109207233+MaysLastPlay10@users.noreply.github.com>
2023-03-20 12:07:13 +0200

    Create HaxeIntro.hx

---
+++ source/HaxeIntro.hx
@@ -0,0 +1,34 @@
+package;
+
+import flixel.util.FlxColor;
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.addons.transition.FlxTransitionableState;
+import FlxVideo;
+
+class HaxeIntro extends MusicBeatState
+{
+    public static var leftState:Bool = false;
+    var video:FlxVideo;
+
+    override function create()
+    {
+        var bg:FlxSprite = new FlxSprite().makeGraphic(FlxG.width, FlxG.height, FlxColor.BLACK);
+		add(bg);
+
+        video = new FlxVideo(Paths.video('HaxeFlixelIntro'));
+        video.finishCallback = () -> skipvideo();
+
+        super.create();
+    }
+
+    function skipvideo() 
+    {
+        video.kill();
+        video.destroy();
+        leftState = true;
+        FlxTransitionableState.skipNextTransIn = true;
+        FlxTransitionableState.skipNextTransOut = true;
+        MusicBeatState.switchState(new TitleState());
+    }
+}
