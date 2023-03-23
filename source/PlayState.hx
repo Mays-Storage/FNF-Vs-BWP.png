b6e851a3dd64510a686418bc2d404364dd3fe99e
MaysLastPlay10 <109207233+MaysLastPlay10@users.noreply.github.com>
2023-03-23 18:20:56 +0200

    okay, that's it

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -67,16 +67,17 @@ class PlayState extends MusicBeatState
 	public static var STRUM_X_MIDDLESCROLL = -278;
 
 	public static var ratingStuff:Array<Dynamic> = [
-		['You Suck!', 0.2], //From 0% to 19%
-		['Shit', 0.4], //From 20% to 39%
-		['Bad', 0.5], //From 40% to 49%
-		['Bruh', 0.6], //From 50% to 59%
-		['Meh', 0.69], //From 60% to 68%
-		['Nice', 0.7], //69%
-		['Good', 0.8], //From 70% to 79%
-		['Great', 0.9], //From 80% to 89%
-		['Sick!', 1], //From 90% to 99%
-		['Perfect!!', 1] //The value on this one isn't used actually, since Perfect is always "1"
+
+		['BWP is disappointed in you...', 0.2], //From 0% to 19%
+		['hmm', 0.4], //From 20% to 39%
+		['whats happening?', 0.5], //From 40% to 49%
+		['bwp is starting to be happy again', 0.6], //From 50% to 59%
+		['Not bad at all!', 0.69], //From 60% to 68%
+		['Pretty Good', 0.7], //69%
+		['Good Job!', 0.8], //From 70% to 79%
+		['Fantastic Job!!', 0.9], //From 80% to 89%
+		['Bwp is satisfied haha', 1], //From 90% to 99%
+		['YOU ARE GOD. BWP APPROVES!!!', 1] //The value on this one isn't used actually, since Perfect is always "1"
 	];
 	public var modchartTweens:Map<String, FlxTween> = new Map<String, FlxTween>();
 	public var modchartSprites:Map<String, ModchartSprite> = new Map<String, ModchartSprite>();
